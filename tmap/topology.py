6b42a5d092407ec1c8baf508c7aab6c3bf5780f1
NicolasDenoyelle <denoyelle.nicolas@gmail.com>
2020-05-10 17:38:53 -0500

    add collection of all object of a type below parent

--- tmap/topology.py
+++ tmap/topology.py
@@ -17,13 +17,68 @@ Use hwloc-info and hwloc-calc command line utilities to
 create a Tree based on a machine topology.
 """
 class Topology(Tree):
+    type_re = re.compile('^\stype\s=\s(?P<i>\w+)$')
+    lindex_re = re.compile('^\slogical\sindex\s=\s(?P<i>\d+)$')
+    osindex_re = re.compile('^\sos\sindex\s=\s(?P<i>\d+)$')
+    memchild_re = re.compile('^\smemory\schildren\s=\s(?P<i>\d+)$')
+    cpuset_re = re.compile('^\scpuset\s=\s(?P<i>(\w+,?)+)$')
+    nodeset_re = re.compile('^\snodeset\s=\s(?P<i>\w+)$')
+
+    @staticmethod
+    def parse_obj_info(info: str):
+        ret = {
+            'logical_index': None,
+            'memory_children': None,
+            'os_index': None,
+            'type': None,
+            'cpuset': None,
+            'nodeset': None
+        }
+        for line in info.split('\n'):
+            match = Topology.type_re.match(line)
+            if match is not None:
+                ret['type'] = match.group(1)                
+            match = Topology.cpuset_re.match(line)
+            if match is not None:
+                ret['cpuset'] = match.group(1)
+            match = Topology.type_re.match(line)
+            if match is not None:
+                ret['nodeset'] = match.group(1)
+            match = Topology.lindex_re.match(line)
+            if match is not None:
+                ret['logical_index'] = int(match.group(1))
+            match = Topology.osindex_re.match(line)
+            if match is not None:
+                ret['os_index'] = int(match.group(1))
+            match = Topology.memchild_re.match(line)
+            if match is not None:
+                ret['memory_children'] = int(match.group(1))
+        return ret
+
+    """
+    Invoke hwloc-calc to retrieve the list of children of @child_type of a topology
+    object of a certain @type and @logical_index.
+    @return [ dict ]
+    """
+    @staticmethod
+    def get_children(type: str, logical_index: int, child_type: str, input_topology=None):
+        cmd = 'hwloc-info'
+        if input_topology is not None:
+            cmd += ' --input {}'.format(input_topology)
+        cmd += ' --descendants {} {}:{}'.format(child_type, type, logical_index)
+        out = subprocess.getstatusoutput(cmd)
+        if out[0] != 0:
+            raise ValueError('Invalid topology file')
+        elements = re.split('\n\w+.*\n', out[1])
+        return [ Topology.parse_obj_info(e) for e in elements ]
+    
     """
     Invoke hwloc-info and parse output to collect some attributes 
     of a topology object.
     @return dict
     """
     @staticmethod
-    def attributes(type: str, logical_index: int, input_topology=None):
+    def get_node(type: str, logical_index: int, input_topology=None):
         cmd = 'hwloc-info'
         if input_topology is not None:
             cmd += ' --input {}'.format(input_topology)
@@ -31,62 +86,8 @@ class Topology(Tree):
         out = subprocess.getstatusoutput(cmd)
         if out[0] != 0:
             raise ValueError('Invalid topology file')
-        
-        lines = out[1].split('\n')
-        ret = {
-            'logical_index': logical_index,
-            'memory_children': 0,
-            'os_index': None,
-            'type': type,
-            'cpuset': None,
-            'nodeset': None
-        }
-
-        type_re = re.compile('^\stype\s=\s(?P<i>\w+)$')
-        lindex_re = re.compile('^\slogical\sindex\s=\s(?P<i>\d+)$')
-        osindex_re = re.compile('^\sos\sindex\s=\s(?P<i>\d+)$')
-        memchild_re = re.compile('^\smemory\schildren\s=\s(?P<i>\d+)$')
-        cpuset_re = re.compile('^\scpuset\s=\s(?P<i>\w+)$')
-        nodeset_re = re.compile('^\snodeset\s=\s(?P<i>\w+)$')
-        
-        for line in lines:
-            match = type_re.match(line)
-            if match is not None:
-                ret['type'] = match.group(1)                
-            match = cpuset_re.match(line)
-            if match is not None:
-                ret['cpuset'] = match.group(1)
-            match = type_re.match(line)
-            if match is not None:
-                ret['nodeset'] = match.group(1)
-            match = lindex_re.match(line)
-            if match is not None:
-                ret['logical_index'] = int(match.group(1))
-            match = osindex_re.match(line)
-            if match is not None:
-                ret['os_index'] = int(match.group(1))
-            match = memchild_re.match(line)
-            if match is not None:
-                ret['memory_children'] = int(match.group(1))
-
-        return ret
-
-    """
-    Invoke hwloc-calc to get the number of children of @child_type of a topology
-    object of a certain @type and @logical_index.
-    @return int
-    """
-    @staticmethod
-    def n_children(type: str, logical_index: int, child_type: str, input_topology=None):
-        cmd = 'hwloc-calc'
-        if input_topology is not None:
-            cmd += ' --input {}'.format(input_topology)
-        cmd += ' {}:{} -N {}'.format(type, logical_index, child_type)
-        out = subprocess.getstatusoutput(cmd)
-        if out[0] != 0:
-            raise ValueError('Invalid types ot topology file')
-        return int(out[1])
-
+        return Topology.parse_obj_info(out[1])
+    
     """
     Invoke 'hwloc-info' to retrieve a list of objects in the topology with 
     their type, count and depth.
@@ -130,26 +131,21 @@ class Topology(Tree):
     If no_io is True, io objects are filtered.
     """
     def __init__(self, structure=True, no_smt=True, no_io=True, input_topology=None):
+        self.input_topology = input_topology
         objects = Topology.objects(structure, no_smt, no_io, input_topology)
         
-        super().__init__(annotation = Topology.attributes('Machine', 0, input_topology))
+        super().__init__(annotation = Topology.get_node('Machine', 0, input_topology))
         self.annotation['special_objs'] = [ { 'depth': o['depth'],
                                               'type': o['type'],
                                               'count': o['count'], }\
                                             for o in objects if o['depth'] < 0 ]
 
         for o in [ o for o in objects[:len(objects)] if o['depth'] > 0 ]:
-            logical_index = 0
             for leaf in TreeIterator(self, lambda n: n.is_leaf()):
-                n = Topology.n_children(leaf.annotation['type'],
-                                        leaf.annotation['logical_index'],
-                                        o['type'], input_topology)
-                leaf.connect_children([
-                    Tree(annotation = Topology.attributes(o['type'],
-                                                          logical_index+i,
-                                                          input_topology))
-                    for i in range(n) ])
-                logical_index += n
+                children = Topology.get_children(leaf.annotation['type'],
+                                                 leaf.annotation['logical_index'],
+                                                 o['type'], input_topology)
+                leaf.connect_children([ Tree(annotation = c) for c in children ])
 
 "Pre initialized current machine topology."
 topology = Topology()
